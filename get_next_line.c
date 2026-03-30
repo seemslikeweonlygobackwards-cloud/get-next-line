e18f20ff1ef046dfacf7e242537921db998b0ba5
Monya <seemslikeweonlygobackwards@gmail.com>
2026-03-30 21:05:51 +0400

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,23 +6,60 @@
 /*   By: mavanesy <mavanesy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/22 16:15:15 by mavanesy          #+#    #+#             */
-/*   Updated: 2026/03/25 19:52:24 by mavanesy         ###   ########.fr       */
+/*   Updated: 2026/03/30 21:05:02 by mavanesy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*read_funciton(char *stash, char *buffer, int fd)
+{
+	int		byte;
+	char	*temp;
+
+	byte = read (fd, buffer, BUFFER_SIZE);
+	if (byte < 0)
+		return (NULL);
+	while (byte && !ft_strchr(buffer, '\n'))
+	{
+		temp = stash;
+		stash = strjoin(stash, buffer);
+		free(temp);
+		byte = read (fd, buffer, BUFFER_SIZE);
+	}
+	temp = stash;
+	stash = strjoin(stash, buffer);
+	free(temp);
+	return (stash);
+}
+
+char	*extraction ()
+{
+	
+}
+char	*stash_update()
+{
+	
+}
+
 char	*get_next_line(int fd)
 {
 	char		*buffer;
 	static char	*stash;
-	static int	i;
+	int			byte;
 
-	i = 0;
+	if (fd < 0 || BUFFER_SIZE < 0)
+		return (NULL);
 	buffer = malloc (BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
-	read (fd, buffer, BUFFER_SIZE);
-	if (ft_strchr(buffer, '\n'))
-		return (buffer);
+	byte = read (fd, buffer, BUFFER_SIZE);
+	if (byte < 0)
+		return (free(buffer), NULL);
+	while (*buffer)
+	{
+		*stash = ft_strjoin(stash, buffer);
+		if (*buffer == "\n")
+			return (ft_strdup(buffer));
+	}
 }
